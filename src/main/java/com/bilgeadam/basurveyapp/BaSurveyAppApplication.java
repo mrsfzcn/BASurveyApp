aa08720301ad498e6b76c577b6b61ab0aa2b2b19
EralpN <eralp.nitelik@gmail.com>
2023-01-10 14:43:07 +0300

    Config

--- src/main/java/com/bilgeadam/basurveyapp/BaSurveyAppApplication.java
+++ src/main/java/com/bilgeadam/basurveyapp/BaSurveyAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BaSurveyAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BaSurveyAppApplication.class, args);
     }
-
 }
