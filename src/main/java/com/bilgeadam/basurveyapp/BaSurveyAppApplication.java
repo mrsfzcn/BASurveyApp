b8f435b3958149a011e975d19b102d24a66b08c0
EralpN <eralp.nitelik@gmail.com>
2023-01-10 11:07:19 +0300

    Initial Commit

---
+++ src/main/java/com/bilgeadam/basurveyapp/BaSurveyAppApplication.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.basurveyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaSurveyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaSurveyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bilgeadam/basurveyapp/BaSurveyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.basurveyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaSurveyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
