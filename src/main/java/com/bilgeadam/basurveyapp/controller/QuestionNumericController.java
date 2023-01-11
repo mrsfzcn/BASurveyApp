d22370ea8cd4ded30452770260b514307ff7137e
BernaBAYKAN <77546837+BernaBAYKAN@users.noreply.github.com>
2023-01-11 16:34:23 +0300

    task-0044-0045-0046 done

--- src/main/java/com/bilgeadam/basurveyapp/controller/QuestionNumericController.java
+++ src/main/java/com/bilgeadam/basurveyapp/controller/QuestionNumericController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/questionnumeric")
 public class QuestionNumericController {
 
-    /*
 
-     */
     private final QuestionNumericService questionNumericService;
 }
--- src/main/java/com/bilgeadam/basurveyapp/services/QuestionNumericService.java
+++ src/main/java/com/bilgeadam/basurveyapp/services/QuestionNumericService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 public class QuestionNumericService extends ServiceManager<QuestionNumeric,Long> {
 
 
+
     /*
 
      */
