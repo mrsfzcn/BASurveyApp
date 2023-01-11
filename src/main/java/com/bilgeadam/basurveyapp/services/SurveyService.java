6355837d309647c8a783ff94a6c5b99a879d09de
elify <elif.yildirim@bilgeadam.com>
2023-01-11 14:16:32 +0300

    Survey Service ve Controller eklendi.

---
+++ src/main/java/com/bilgeadam/basurveyapp/services/SurveyService.java
@@ -0,0 +1,15 @@
+package com.bilgeadam.basurveyapp.services;
+
+import com.bilgeadam.basurveyapp.repository.ISurveyRepository;
+import com.bilgeadam.basurveyapp.utility.ServiceManager;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SurveyService extends ServiceManager<Survey,Long> {
+    private final ISurveyRepository surveyRepository;
+
+    public SurveyService(ISurveyRepository surveyRepository) {
+        super(surveyRepository);
+        this.surveyRepository = surveyRepository;
+    }
+}
