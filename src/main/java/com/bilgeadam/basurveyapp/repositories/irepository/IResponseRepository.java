7f660ec6ed73272bd34a752db853a7e0decaf6bc
EralpN <eralp.nitelik@gmail.com>
2023-01-18 15:12:23 +0300

    Added a query

--- src/main/java/com/bilgeadam/basurveyapp/repositories/irepository/IResponseRepository.java
+++ src/main/java/com/bilgeadam/basurveyapp/repositories/irepository/IResponseRepository.java
@@ -2,11 +2,14 @@ package com.bilgeadam.basurveyapp.repositories.irepository;
 
 import com.bilgeadam.basurveyapp.entity.Response;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface IResponseRepository extends JpaRepository<Response, Long> {
-
-
+    @Query("SELECT r FROM Response r WHERE r.user.oid = ?1 AND r.question.oid = ?2")
+    Optional<Response> findByUserOidAndQuestionOid(Long userOid, Long questionOid);
 }
 
