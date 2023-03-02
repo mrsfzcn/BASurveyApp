ce1fdb719e7291c7949c3eb260bd18fc3ddb651e
Semih Biygit <semihbiygit@gmail.com>
2023-03-02 18:26:28 +0300

    tarayıcı diline göre dil değişimi

--- src/main/java/com/bilgeadam/basurveyapp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/bilgeadam/basurveyapp/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.bilgeadam.basurveyapp.exceptions;
 
 import com.bilgeadam.basurveyapp.exceptions.custom.*;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +17,7 @@ import java.util.Locale;
 
 import static com.bilgeadam.basurveyapp.exceptions.ExceptionType.*;
 
-/**
- * @author Eralp Nitelik
- */
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -31,135 +30,134 @@ public class GlobalExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception exception) {
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception exception, HttpServletRequest request) {
         log.error(messageSource.getMessage("exception.UNEXPECTED_ERROR=", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(UNEXPECTED_ERROR, exception);
+        return createExceptionInfoResponse(UNEXPECTED_ERROR, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ExceptionResponse> handleRuntimeException(RuntimeException exception) {
+    public ResponseEntity<ExceptionResponse> handleRuntimeException(RuntimeException exception, HttpServletRequest request) {
         log.error(messageSource.getMessage("exception.RUNTIME_ERROR", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(RUNTIME_EXCEPTION, exception);
+        return createExceptionInfoResponse(RUNTIME_EXCEPTION, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> handleRuntimeException(UsernameNotFoundException exception) {
+    public ResponseEntity<ExceptionResponse> handleRuntimeException(UsernameNotFoundException exception, HttpServletRequest request) {
         String errorMessage = messageSource.getMessage("exception.LOGIN_ERROR_USERNAME_DOES_NOT_EXIST", null, Locale.getDefault());
         log.error(errorMessage, exception);
-        return createExceptionInfoResponse(LOGIN_ERROR_USERNAME_DOES_NOT_EXIST, exception);
+        return createExceptionInfoResponse(LOGIN_ERROR_USERNAME_DOES_NOT_EXIST, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ExceptionResponse> handleBadCredentialsException(BadCredentialsException exception) {
+    public ResponseEntity<ExceptionResponse> handleBadCredentialsException(BadCredentialsException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.LOGIN_ERROR_WRONG_PASSWORD", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(LOGIN_ERROR_WRONG_PASSWORD, exception);
+        return createExceptionInfoResponse(LOGIN_ERROR_WRONG_PASSWORD, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<ExceptionResponse> handleUserAlreadyExistsException(UserAlreadyExistsException exception) {
+    public ResponseEntity<ExceptionResponse> handleUserAlreadyExistsException(UserAlreadyExistsException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.REGISTER_ERROR_DATA_EXISTS", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(REGISTER_ERROR_DATA_EXISTS, exception);
+        return createExceptionInfoResponse(REGISTER_ERROR_DATA_EXISTS, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> handleResourceNotFoundException(ResourceNotFoundException exception) {
+    public ResponseEntity<ExceptionResponse> handleResourceNotFoundException(ResourceNotFoundException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.RESOURCE_NOT_FOUND", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(RESOURCE_NOT_FOUND, exception);
+        return createExceptionInfoResponse(RESOURCE_NOT_FOUND, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.DATA_NOT_VALID", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(DATA_NOT_VALID, exception);
+        return createExceptionInfoResponse(DATA_NOT_VALID, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<ExceptionResponse> handleAccessDeniedException(AccessDeniedException exception) {
+    public ResponseEntity<ExceptionResponse> handleAccessDeniedException(AccessDeniedException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.ACCESS_DENIED", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(ACCESS_DENIED, exception);
+        return createExceptionInfoResponse(ACCESS_DENIED, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(QuestionNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> handleQuestionNotFoundException(QuestionNotFoundException exception) {
+    public ResponseEntity<ExceptionResponse> handleQuestionNotFoundException(QuestionNotFoundException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.QUESTION_NOT_FOUND", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(QUESTION_NOT_FOUND, exception);
+        return createExceptionInfoResponse(QUESTION_NOT_FOUND, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(QuestionTypeNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> handleQuestionTypeNotFoundException(QuestionTypeNotFoundException exception) {
+    public ResponseEntity<ExceptionResponse> handleQuestionTypeNotFoundException(QuestionTypeNotFoundException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.QUESTION_TYPE_NOT_FOUND", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(QUESTION_TYPE_NOT_FOUND, exception);
+        return createExceptionInfoResponse(QUESTION_TYPE_NOT_FOUND, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(AlreadyAnsweredSurveyException.class)
-    public ResponseEntity<ExceptionResponse> handleAlreadyAnsweredSurveyException(AlreadyAnsweredSurveyException exception) {
+    public ResponseEntity<ExceptionResponse> handleAlreadyAnsweredSurveyException(AlreadyAnsweredSurveyException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.SURVEY_ALREADY_ANSWERED", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(SURVEY_ALREADY_ANSWERED, exception);
+        return createExceptionInfoResponse(SURVEY_ALREADY_ANSWERED, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(QuestionsAndResponsesDoesNotMatchException.class)
-    public ResponseEntity<ExceptionResponse> handleQuestionsAndResponsesDoesNotMatchException(QuestionsAndResponsesDoesNotMatchException exception) {
+    public ResponseEntity<ExceptionResponse> handleQuestionsAndResponsesDoesNotMatchException(QuestionsAndResponsesDoesNotMatchException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.QUESTIONS_AND_RESPONSES_DOES_NOT_MATCH", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(QUESTIONS_AND_RESPONSES_DOES_NOT_MATCH, exception);
+        return createExceptionInfoResponse(QUESTIONS_AND_RESPONSES_DOES_NOT_MATCH, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(UserInsufficientAnswerException.class)
-    public ResponseEntity<ExceptionResponse> handleUserInsufficientAnswerException(UserInsufficientAnswerException exception) {
+    public ResponseEntity<ExceptionResponse> handleUserInsufficientAnswerException(UserInsufficientAnswerException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.USER_INSUFFICIENT_ANSWER", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(USER_INSUFFICIENT_ANSWER, exception);
+        return createExceptionInfoResponse(USER_INSUFFICIENT_ANSWER, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(UserDoesNotExistsException.class)
-    public ResponseEntity<ExceptionResponse> handleUserDoesNotExistsException(UserDoesNotExistsException exception) {
+    public ResponseEntity<ExceptionResponse> handleUserDoesNotExistsException(UserDoesNotExistsException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.USER_DOES_NOT_EXIST", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(USER_DOES_NOT_EXIST, exception);
+        return createExceptionInfoResponse(USER_DOES_NOT_EXIST, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(SurveyNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> handleSurveyNotFoundException(SurveyNotFoundException exception) {
+    public ResponseEntity<ExceptionResponse> handleSurveyNotFoundException(SurveyNotFoundException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.SURVEY_NOT_FOUND", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(SURVEY_NOT_FOUND, exception);
+        return createExceptionInfoResponse(SURVEY_NOT_FOUND, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(ClassroomNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> handleClassroomNotFoundException(ClassroomNotFoundException exception) {
+    public ResponseEntity<ExceptionResponse> handleClassroomNotFoundException(ClassroomNotFoundException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.CLASSROOM_NOT_FOUND", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(CLASSROOM_NOT_FOUND, exception);
+        return createExceptionInfoResponse(CLASSROOM_NOT_FOUND, exception, request);
     }
 
     @ResponseBody
     @ExceptionHandler(ClassroomExistException.class)
-    public ResponseEntity<ExceptionResponse> handleClassroomExistException(ClassroomExistException exception) {
+    public ResponseEntity<ExceptionResponse> handleClassroomExistException(ClassroomExistException exception, HttpServletRequest request) {
         log.warn(messageSource.getMessage("exception.CLASSROOM_ALREADY_EXISTS", null, Locale.getDefault()), exception);
-        return createExceptionInfoResponse(CLASSROOM_ALREADY_EXISTS, exception);
+        return createExceptionInfoResponse(CLASSROOM_ALREADY_EXISTS, exception, request);
     }
 
-    //    private ResponseEntity<ExceptionResponse> createExceptionInfoResponse(ExceptionType exceptionType, Exception exception) {
-//        return new ResponseEntity<>(ExceptionResponse.builder()
-//                .exceptionCode(exceptionType.getCode())
-//                .customMessage(exceptionType.getMessage())
-//                .exceptionMessage(exception.getMessage())
-//                .httpStatus(exceptionType.getHttpStatus().value())
-//                .build(), exceptionType.getHttpStatus());
-//    }
-    private ResponseEntity<ExceptionResponse> createExceptionInfoResponse(ExceptionType exceptionType, Exception exception) {
+    private ResponseEntity<ExceptionResponse> createExceptionInfoResponse(ExceptionType exceptionType, Exception exception, HttpServletRequest request) {
         String exceptionName = exceptionType.name();
-        String localizedMessage = messageSource.getMessage("exception." + exceptionName, null, Locale.getDefault());
+        Locale locale;
+        String acceptLanguage = request.getHeader("Accept-Language");
+        if (acceptLanguage != null && acceptLanguage.toLowerCase().startsWith("tr")) {
+            locale = Locale.getDefault();
+        } else {
+            locale = Locale.US;
+        }
+        String localizedMessage = messageSource.getMessage("exception." + exceptionName, null, locale);
         return new ResponseEntity<>(ExceptionResponse.builder()
                 .exceptionCode(exceptionType.getCode())
                 .customMessage(localizedMessage)
@@ -167,6 +165,5 @@ public class GlobalExceptionHandler {
                 .httpStatus(exceptionType.getHttpStatus().value())
                 .build(), exceptionType.getHttpStatus());
     }
-
 
 }
