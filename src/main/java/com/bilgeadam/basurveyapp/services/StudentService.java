7445f4d5fd2488b58d974badbbe651c4b07a46fb
Kaan Kara <kaankara1987@gmail.com>
2023-03-08 14:50:51 +0300

    user entitysini bölüp gerekli student, trainer ve manager entityleri ve gerekli repository, service ve controllerın iskeleti yazıldı

--- src/main/java/com/bilgeadam/basurveyapp/services/StudentService.java
+++ src/main/java/com/bilgeadam/basurveyapp/services/StudentService.java
@@ -1,5 +1,6 @@
 package com.bilgeadam.basurveyapp.services;
 
+import com.bilgeadam.basurveyapp.entity.Student;
 import com.bilgeadam.basurveyapp.repositories.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class StudentService {
     private final StudentRepository studentRepository;
+
+    public Boolean createStudent(Student student) {
+        studentRepository.save(student);
+        return true;
+    }
 }
