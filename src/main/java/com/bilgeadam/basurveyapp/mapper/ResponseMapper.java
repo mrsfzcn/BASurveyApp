6753064ead177a4cbfb4889d09222735cc41342c
Eda Senem BIYIK <96740740+edasenembiyik@users.noreply.github.com>
2023-03-15 00:44:53 +0300

    Response Service e mapper entegre edildi Response controller da hatalı endpoind düzeltildi Response Mapper Class ı oluşturuldu

---
+++ src/main/java/com/bilgeadam/basurveyapp/mapper/ResponseMapper.java
@@ -0,0 +1,26 @@
+package com.bilgeadam.basurveyapp.mapper;
+
+import com.bilgeadam.basurveyapp.dto.request.ResponseRequestSaveDto;
+import com.bilgeadam.basurveyapp.dto.response.AnswerResponseDto;
+import com.bilgeadam.basurveyapp.entity.Question;
+import com.bilgeadam.basurveyapp.entity.Response;
+import com.bilgeadam.basurveyapp.entity.User;
+import org.aspectj.weaver.patterns.TypePatternQuestions;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ResponseMapper {
+    ResponseMapper INSTANCE = Mappers.getMapper(ResponseMapper.class);
+    List<AnswerResponseDto> toAnswerResponseDto(final List<Response> responseDtoList);
+    @Mapping(target = "userOid", source = "response.user.oid")
+    @Mapping(target = "questionOid", source = "response.question.oid")
+    AnswerResponseDto toAnswerResponseDto(final Response response);
+
+    Response toResponse(final ResponseRequestSaveDto responseRequestSaveDto, final Question question, final User user);
+}
