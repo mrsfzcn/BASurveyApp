0e9530117a8c18da58c26e910da749feff328517
aliogutcen <aliogutcen@gmail.com>
2023-07-18 12:03:09 +0300

    Task-648 // input

---
+++ BASurvayFrontend/src/components/Input.jsx
@@ -0,0 +1,37 @@
+import classNames from "classnames";
+
+function Input({
+  value,
+  type,
+  placeholder,
+  fit,
+  full,
+  half,
+  disabled,
+  required,
+  ...rest
+}) {
+  const classes = classNames(
+    rest.className,
+    "p-2 text-lg bg-transparent border-b border-black   outline-none text-black transition disabled:bg-neutral-900 disabled:opacity-70 disabled:cursor-not-allowed",
+    {
+      "w-fit": fit,
+      "w-full": full,
+      "w-1/2": half,
+    }
+  );
+
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={rest.onChange}
+      className={classes}
+      disabled={disabled}
+      required={required}
+    />
+  );
+}
+
+export default Input;
