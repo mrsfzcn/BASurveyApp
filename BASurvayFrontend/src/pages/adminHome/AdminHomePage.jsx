003cbbdba3ccd68da5cb8ee7fbf15438e8357a9c
enesdoganoglu <117980244+enesdoganoglu@users.noreply.github.com>
2023-07-19 22:46:13 +0300

    Adminhome page sayfasına content eklendi.

--- BASurvayFrontend/src/pages/adminHome/AdminHomePage.jsx
+++ BASurvayFrontend/src/pages/adminHome/AdminHomePage.jsx
@@ -2,6 +2,12 @@
 import React, { useState } from "react";
 import Layout from "../../components/Layout";
 import Dropdown from "../../components/Dropdown";
+import "../adminHome/adminhomepage.css";
+import ReportingSvg from "./reportingSvg";
+import ClassroomSvg from "./classroomSvg";
+import SurveySvg from "./surveySvg";
+import UserGroupSvg from "./userGroupSvg";
+import QuestionSvg from "./questionSvg";
 
 function AdminHome() {
   const [name, setName] = useState("");
@@ -9,6 +15,37 @@ function AdminHome() {
 
   const handleChange = (e) => {
     setName(e.target.value);
+  };
+
+  const handleUserClick = (event) => {
+    const target = event.target;
+      console.log("User area clicked!");
+    
+  };
+
+  const handleSurveyClick = (event) => {
+    const target = event.target;
+    
+      console.log("Survey area clicked!");
+    
+  };
+  const handleReportingClick = (event) => {
+    const target = event.target;
+    
+      console.log("Reporting area clicked!");
+   
+  };
+  const handleQuestionClick = (event) => {
+    const target = event.target;
+    
+      console.log("Question area clicked!");
+   
+  };
+  const handleClassroomClick = (event) => {
+    const target = event.target;
+   
+      console.log("Classroom area clicked!");
+  
   };
 
   const options = [
@@ -27,6 +64,26 @@ function AdminHome() {
         <Dropdown value={selection} onChange={onChange} options={options} />
         <Dropdown value={selection} onChange={onChange} options={options} />
       </div>
+      <div className="question-container" onClick={handleQuestionClick}>
+   <QuestionSvg/>
+    </div>
+    <div className="classroom-container" onClick={handleClassroomClick}>
+    <ClassroomSvg/>
+    </div>
+    <div className="survey-container" onClick={handleSurveyClick}>
+    <SurveySvg/> 
+    </div>
+    <div className="user-container" onClick={handleUserClick}>  
+    <UserGroupSvg/> 
+    </div>
+    <div className="circle-reporting" onClick={handleReportingClick}> 
+    <div className="svg-reporting" >
+      <ReportingSvg/>
+      </div>
+      <div>
+      <p>Raporlama İşlemleri</p>
+      </div>
+    </div>
     </Layout>
   );
 }
