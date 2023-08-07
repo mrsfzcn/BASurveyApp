84cf2813677ff11544a0bbee93c659b4c521dbb1
mertnamsal <namsalmert@gmail.com>
2023-08-07 15:39:29 +0300

    task-722 survey daha önce cevaplandıysa anket silinmemesi saglandi.

---
+++ src/main/java/com/bilgeadam/basurveyapp/exceptions/custom/SurveyAnsweredException.java
@@ -0,0 +1,7 @@
+package com.bilgeadam.basurveyapp.exceptions.custom;
+
+public class SurveyAnsweredException extends RuntimeException{
+    public SurveyAnsweredException(String message){
+        super(message);
+    }
+}
