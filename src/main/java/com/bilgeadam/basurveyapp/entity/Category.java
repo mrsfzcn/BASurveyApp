992e617ca721e3805830895c86317abd6773c742
engin.akalin <engin.akalin@bilgeadam.com>
2023-12-01 13:40:09 +0300

    Category class'ı oluşturuldu.Kelime arama ve ekleme işlemleri için TrieNode class'ı oluşturuldu.

---
+++ src/main/java/com/bilgeadam/basurveyapp/entity/Category.java
@@ -0,0 +1,20 @@
+package com.bilgeadam.basurveyapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
